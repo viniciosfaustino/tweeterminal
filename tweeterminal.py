a2235ee2a3ccfb86254c1fec67fe20ab6c419267
viniciosfaustino <vinicioskf@gmail.com>
2020-09-24 14:32:20 -0400

    inicial

---
+++ tweeterminal.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+import os
+import sys
+import tweepy
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+consumer_key = os.getenv('CONSUMER_KEY')
+consumer_secret = os.getenv('CONSUMER_SECRET')
+access_token = os.getenv('ACCESS_TOKEN')
+access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+stamp = '#tweeterminal '
+
+tweet = sys.argv[1]
+final_tweet = stamp + tweet
+print(final_tweet)
+#api.update_status(final_tweet)
